9c3a28785c76356d028850a090615b01a3fe6812
Mokerikun <58637721+Mokerikun@users.noreply.github.com>
2020-10-23 10:28:15 +0300

    Update chams.hpp

--- CSGOSimple/features/chams.hpp
+++ CSGOSimple/features/chams.hpp
@@ -31,4 +31,12 @@ private:
     IMaterial* materialRegular = nullptr;
     IMaterial* materialFlat = nullptr;
     IMaterial* materialGlow = nullptr;
+
+    constexpr auto change_material(int material) {
+        switch (material) {
+        case 0: return materialRegular;
+        case 1: return materialFlat;
+        case 2: return materialGlow;
+        }
+    }
 };
