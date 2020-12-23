0ff7b94368d4ef6af1ccbc709f92ab260fa9a8e5
Mokerikun <58637721+Mokeri@users.noreply.github.com>
2020-12-23 19:28:41 +0300

    Update render.hpp

--- akenaSDK/render/render.hpp
+++ akenaSDK/render/render.hpp
@@ -50,43 +50,6 @@ public:
 	}
 
 	template <class T>
-	inline void RenderBoxByType(T x1, T y1, T x2, T y2, Color color, float thickness = 1.f, int type = 0) {
-		if (type == 0)
-			RenderBox(x1, y1, x2, y2, color, thickness);
-		else if (type == 1)
-			RenderCoalBox(x1, y1, x2, y2, color);
-		else if (type == 2)
-			RenderBox(x1, y1, x2, y2, color, thickness, 8.f);
-	}
-
-	template <class T>
-	inline void RenderBoxFilledByType(T x1, T y1, T x2, T y2, Color color, float thickness = 1.f, int type = 0) {
-		if (type == 0 || type == 1)
-			RenderBoxFilled(x1, y1, x2, y2, color, thickness);
-		else if (type == 2)
-			RenderBoxFilled(x1, y1, x2, y2, color, thickness, 8.f);
-	}
-
-	template <class T>
-	inline void RenderCoalBox(T x1, T y1, T x2, T y2, Color color, float th = 1.f) {
-		int w = x2 - x1;
-		int h = y2 - y1;
-
-		int iw = w / 4;
-		int ih = h / 4;
-		// top
-		RenderLine(x1, y1, x1 + iw, y1, color, th);					// left
-		RenderLine(x1 + w - iw, y1, x1 + w, y1, color, th);			// right
-		RenderLine(x1, y1, x1, y1 + ih, color, th);					// top left
-		RenderLine(x1 + w - 1, y1, x1 + w - 1, y1 + ih, color, th);	// top right
-																	// bottom
-		RenderLine(x1, y1 + h, x1 + iw, y1 + h, color, th);			// left
-		RenderLine(x1 + w - iw, y1 + h, x1 + w, y1 + h, color, th);	// right
-		RenderLine(x1, y1 + h - ih, x1, y1 + h, color, th);			// bottom left
-		RenderLine(x1 + w - 1, y1 + h - ih, x1 + w - 1, y1 + h, color, th);	// bottom right
-	}
-
-	template <class T>
 	inline void RenderBox(T x1, T y1, T x2, T y2, Color color, float thickness = 1.f, float rounding = 0.f) {
 		draw_list->AddRect(ImVec2(x1, y1), ImVec2(x2, y2), GetU32(color), rounding, 15, thickness);
 	}
